13746ffdf6c98b36f88b79629c7eb6f58e0cf1fb
deidg <64797316+deidg@users.noreply.github.com>
2022-11-18 17:16:15 +0300

    commit 6 - screen 1.1. done, comments cleaned.    ### ADDED

--- Vlad_1_Reverse words/ViewController.swift
+++ Vlad_1_Reverse words/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     var displayButton = UIButton()
+    let largeLabel = UILabel()
     let mainLabel = UILabel()
     var textField = UITextField()
     let divider = UILabel()
@@ -20,32 +21,32 @@ class ViewController: UIViewController {
     self.view.backgroundColor = UIColor (red: 0.898, green: 0.898, blue: 0.898, alpha: 1)
             
         displayButton.translatesAutoresizingMaskIntoConstraints = false
-        
-//        let screenSize: CGRect = UIScreen.main.bounds
-        
+                
         //title for Navigation Controller
         self.title = "Reverse words"
-//        self.title
+
+        
+        //largeLabel
+        largeLabel.frame = CGRect(x: 16, y: 152, width: 343, height: 41);
+        largeLabel.font = UIFont.systemFont(ofSize: 34, weight: .bold)   // weight from CSS
+        largeLabel.textAlignment = .center
+        largeLabel.text = "Reverse words"
+
+        self.view.addSubview(largeLabel)
 
         
         //mainLabel
-//        let mainLabel = UILabel() - дубль?
-//        let margins = view.layoutMarginsGuide
-        mainLabel.frame = CGRect(x: 140, y: 100, width:150, height:30);
-        mainLabel.font = UIFont.systemFont(ofSize: 35)
-        mainLabel.text = "Reverse words"
-
+        mainLabel.frame = CGRect(x: 33, y: 209, width: 308, height: 44);
+        mainLabel.font = UIFont.systemFont(ofSize: 17)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+        mainLabel.text = "This application will reverse your words. Please type text below"
+        //mainLabel.textColor = UIColor(red: 60, green: 60, blue: 67, alpha: 1) -  not settled (from CSS code)
+        
         self.view.addSubview(mainLabel)
         
         
-        //====
-        
         //TextField
-                
-//        textField.text = "Text to reverse"
-        //        textField.didSetText = false
-        //        var textField = UITextField()
-
         textField.frame = CGRect(x: 16, y: 312, width: 343, height: 22)
         textField.font = UIFont.systemFont(ofSize: 17)
         textField.text = nil
@@ -53,208 +54,30 @@ class ViewController: UIViewController {
         
         self.view.addSubview(textField)
         
-//        ????
-//        placeholderTextField.isUserInteractionEnabled = false
-//                placeholderTextField.adjustsFontSizeToFitWidth = true
 
-//====
-        
-//        divider
-        
+        //divider
         self.divider.frame = CGRect(x: 16, y: 352.5, width: 343, height: 0.5)
-//        self.divider.backgroundColor = UIColor()
-
-        
         self.divider.backgroundColor = UIColor(red: 34, green: 34, blue: 34, alpha: 1)
-        
-//        (red: 0.898, green: 0.898, blue: 0.898, alpha: 1)
-        
-        
+
         self.view.addSubview(divider)
 
-//        =====
-        
-      //   displayButton
+
+        //displayButton
         self.displayButton = UIButton(type: .system)
-       
         self.displayButton.setTitle("Reverse", for: .normal)
         self.displayButton.setTitleColor(.white, for: .normal)
-//        self.displayButton.titleLabel?.font = UIFont.
         self.displayButton.titleLabel?.font = UIFont.systemFont(ofSize: 17)
-
-//        self.displayButton.frame.size.width = 349
-//        self.displayButton.frame.size.height = 60
-//            let margins = displayButton.layoutMarginsGuide
-//        displayButton.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
-
         self.displayButton.layer.cornerRadius = 14
         self.displayButton.backgroundColor = UIColor(red: 0.0, green: 122.0, blue: 255.0, alpha: 1.0)
         self.displayButton.frame = CGRect(x: 13, y: 686, width: 349, height:60);
 
         self.view.addSubview(self.displayButton)
 
-//        =====
-        
-        
-       
-        
-        
-        
-        
-        
-        
 
-//        func createDisplayButtonConstraints() {
-//        NSLayoutConstraint(item: displayButton,
-//                                     attribute: .leading,
-//                                     relatedBy: .equal,
-//                                     toItem: view,
-//                                     attribute: .leadingMargin,
-//                                     multiplier: 1,
-//                                     constant: 0).isActive = true
-//
-//            NSLayoutConstraint(item: displayButton,
-//                                         attribute: .trailingMargin,
-//                                         relatedBy: .equal,
-//                                         toItem: view,
-//                                         attribute: .trailingMargin,
-//                                         multiplier: 1,
-//                                         constant: 0).isActive = true
-//
-//            NSLayoutConstraint(item: displayButton,
-//                                         attribute: .top,
-//                                         relatedBy: .equal,
-//                                         toItem: view,
-//                                         attribute: .topMargin,
-//                                         multiplier: 1,
-//                                         constant: 88).isActive = true
-//
-//            NSLayoutConstraint(item: displayButton,
-//                                         attribute: .height,
-//                                         relatedBy: .equal,
-//                                         toItem: view,
-//                                         attribute: .width,
-//                                         multiplier: 1,
-//                                         constant: 0).isActive = true
-//              }
-        
-        
-//        createDisplayButtonConstraints()
-        
-        
-//         func setConstraints() {
-//                NSLayoutConstraint.activate([
-////                    displayButton.topAnchor.constraint(equalTo: topAnchor, constant: 10),
-//                    displayButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
-////                    displayButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
-//                    displayButton.bottomAnchor.constraint(equalTo: textLabel.topAnchor, constant: 20),
-//                ])
-        
-        
-        
-        
-        
-        
-//        self.displayButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 10)
-
-        
-        
-//        addWorkoutButton.topAnchor.constraint(equalTo: calendarView.bottomAnchor, constant: 5),
-//        addWorkoutButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-//
-//        weatherView.topAnchor.constraint(equalTo: calendarView.bottomAnchor, constant: 5),
-//        weatherView.leadingAnchor.constraint(equalTo: addWorkoutButton.trailingAnchor, constant: 5),
-//        weatherView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-        
-
-        //===
-
-//        self.displayButton.bottomAnchor.constraint(equalTo: <#T##NSLayoutAnchor<NSLayoutYAxisAnchor>#>)
-
-        
-//        displayButton.frame = CGRect.init(x: self.view.frame.width/3.5, y: self.view.frame.height/2,
-//                                          width: 349, height: 60)
-//        self.displayButton.sizeToFit()
-//        self.displayButton.center = self.view.center //расположение кнопки по центру.
-//        self.displayButton.size
-
-        
-//        Int(screenSize.width - 60)
-//        displayButton.frame = CGRect(x: 150, y: 300, width: 349, height: 60)
-//        displayButton.widthAnchor.constraint(equalToConstant: 44).isActive = true
-
-                                      
-        
-//        self.view.addSubview(self.displayButton)
-//        self.view.addSubview(self.mainLabel)
-        
-        
-        
-        
-        
-        
-//        setupConstraints()
-//        createDisplayButtonConstraints()
-        
-        
-        
-        
-//         func setupConstraints() {
-//            displayButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 20).isActive = true
-//        }
-//        func createDisplayButtonConstraints() {
-//            NSLayoutConstraint.activate([
-//                displayButton.leadingAnchor.constraint(equalTo: self.)
-//            ])
-//
-////
-        
-        //Reverse Button
-        
-//
-//        displayButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        
-//        self.displayButton.layer = self.view.center
-        
-        
-        
-//        self.displayButton = UIButton(type: .system)
-//        self.displayButton.backgroundColor = .red
-        
-
-//        self.displayButton.frame.size.width = 200
-//        self.displayButton.frame.size.height = 200
-//        self.displayButton.frame = CGRect.init(x: self.view.frame.width/3.5, y: self.view.frame.height/2, width: 180, height: 50)
-        
-        
-        
-        
-        
-        
-        //TODO
-//        displayButton.layer.borderColor = UIColor.black.cgColor
-//        displayButton.layer.borderWidth = 1.5
-//        displayButton.layer.cornerRadius = 5
-        
-        
-        
-        
-//        self.displayButton.addTarget(self, action: #selector(<#T##@objc method#>), for: .touchUpOutside)
-        
-        
-        
-//        displayButton.addTarget(self, action: #selector(self.buttonTapped), for: .touchUpInside)
-//
-//        @objc func buttonTapped(sender : UIButton) {
-//        //Write button action here
-//        print(“button clicked”)
-        
-        
-        
+    
     }
 
-    }
+}
 
     
 
