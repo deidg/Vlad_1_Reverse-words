17ea37cc3336bf86b99ac4d6c1973b3d7744e075
deidg <64797316+deidg@users.noreply.github.com>
2023-01-01 20:57:37 +0300

    commit 26.2

--- Vlad_1_Reverse words/bull.swift
+++ Vlad_1_Reverse words/bull.swift
@@ -17,3 +17,25 @@ class ViewController: UIViewController {
 func characterCompare(stringOne: String, stringTwo: String) -> Bool {
     return Set(stringOne) == Set(stringTwo)
 }
+
+
+
+// большая статья про функции. Swiftblog
+     https://swiftblog.org/funkcii-v-swift/
+
+статья на хабре - https://habr.com/ru/post/244487/
+
+//метод для перезагрузки изображения (при переиспользовании ячейки чтобы не загружалось старое)
+override func prepareForReuse() {  //переписываем метод
+    super.prepareForReuse()     //обязательно указываем переписываем метод
+    self.productImage.image = nil
+    
+}
+
+    // ставит наблюдателя за данными которые придут от datepicker.  в этом же видео ранее говорит о #selector
+https://youtu.be/TNHXtM7bDiY?t=720
+
+
+
+//программно замена outlets (их протягиванию) - объявление функций @objc
+https://youtu.be/Ime8NK5NLgc?t=258
