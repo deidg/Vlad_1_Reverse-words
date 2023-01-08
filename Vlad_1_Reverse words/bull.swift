fc45d62a7f42f962b944dbc8e4d7d32ce5240925
deidg <64797316+deidg@users.noreply.github.com>
2023-01-08 20:37:03 +0300

    commit 26.2.7

--- Vlad_1_Reverse words/bull.swift
+++ Vlad_1_Reverse words/bull.swift
@@ -413,3 +413,72 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
 
 
+class ViewController: UIViewController {
+    
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.backgroundColor = .white
+        scrollView.frame = self.view.bounds
+        scrollView.contentSize = contentSize
+        return scrollView
+    }()
+    
+    private lazy var contentView: UIView = {
+        let contentView = UIView()
+        contentView.backgroundColor = .white
+        contentView.frame.size = contentSize
+        return contentView
+    }()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 20
+        return stackView
+    }()
+    
+    private var contentSize: CGSize {
+        CGSize(width: view.frame.width, height: view.frame.height + 400)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(stackView)
+        setupColors()
+        setupViewsConstraints()
+    }
+}
+
+
+//MARK: - Setup Views
+extension ViewController {
+    private func setupViewsConstraints() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stackView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            stackView.leftAnchor.constraint(equalTo: contentView.leftAnchor)
+        ])
+        
+        for view in stackView.arrangedSubviews {
+            NSLayoutConstraint.activate([
+                view.widthAnchor.constraint(equalToConstant: 300),
+                view.heightAnchor.constraint(equalToConstant: 100),
+            ])
+        }
+    }
+    
+    private func setupColors() {
+        let colors = [UIColor.red, .green, .blue]
+        for index in 0..<10 {
+            let view = UIView()
+            view.backgroundColor = colors[index % colors.count]
+            stackView.addArrangedSubview(view)
+        }
+    }
+}
