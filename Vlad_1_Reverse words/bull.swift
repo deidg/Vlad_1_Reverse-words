84cae9a287cb8004570dce931a9760457a15bf63
deidg <64797316+deidg@users.noreply.github.com>
2023-01-11 20:26:09 +0300

    commit 26.2.11

--- Vlad_1_Reverse words/bull.swift
+++ Vlad_1_Reverse words/bull.swift
@@ -132,6 +132,31 @@ https://youtu.be/bMIY4ojoh5g?t=1440
 // включить клаву в Симуляторе
 // shift + cmd + K
 
+// MARK: - Game State
+extension QuizViewController {
+  struct State {
+    let questions: [Question]
+    let numberOfQuestions: Int
+    var currentQuestion: Int
+    var correctAnswers: Int
+    
+    init(questions: [Question]) {
+      self.questions = questions
+      numberOfQuestions = questions.count
+      currentQuestion = 1
+      correctAnswers = 0
+    }
+    
+    var isLastQuestion: Bool {
+      return currentQuestion == numberOfQuestions
+    }
+    
+    var question: Question {
+      return questions[currentQuestion - 1]
+    }
+  }
+}
+
 
 extension ViewController: UITextFieldDelegate {
 
