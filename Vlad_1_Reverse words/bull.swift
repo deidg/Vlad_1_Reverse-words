8113c268ee1047212a0e64485a03522329c57c89
deidg <64797316+deidg@users.noreply.github.com>
2023-01-11 21:42:23 +0300

    commit 26.2.12

--- Vlad_1_Reverse words/bull.swift
+++ Vlad_1_Reverse words/bull.swift
@@ -318,6 +318,62 @@ class BullsEyeMockTests: XCTestCase {
     sut.defaults = mockUserDefaults
   }
 
+    
+    
+    class PhotosCollectionViewController: UICollectionViewController {
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+
+        }
+        
+        // MARK: UICollectionViewDataSource
+
+        override func numberOfSections(in collectionView: UICollectionView) -> Int {
+            // #warning Incomplete implementation, return the number of sections
+            return 1
+        }
+
+
+        override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            // #warning Incomplete implementation, return the number of items
+            return 15
+        }
+
+        override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
+        
+            cell.backgroundColor = .black
+        
+            return cell
+        }
+    }
+
+    extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            let itemsPerRow: CGFloat = 3
+            let paddingWidth = 20 * (itemsPerRow + 1)
+            let availableWidth = collectionView.frame.width - paddingWidth
+            let widthPerItem = availableWidth / itemsPerRow
+            return CGSize(width: widthPerItem, height: widthPerItem)
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+            return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+            return 20
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+            return 20
+        }
+        
+        
+    }
+
   override func tearDownWithError() throws {
     sut = nil
     mockUserDefaults = nil
